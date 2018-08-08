2c07b0b207ed19afae477bd2bd1820f387010d06
krzjablonski <40863404+krzjablonski@users.noreply.github.com>
2018-08-08 20:17:23 +0200

    mailtrap

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'noreply@bigsite.pl'),
         'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 
